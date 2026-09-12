4105accc85b7a49c62c22c5d9f28293bda23ee3c
William Lu <32563160+williaml12@users.noreply.github.com>
2026-09-11 23:32:13 -0400

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,28 +1,3 @@
-# # SPDX-FileCopyrightText: Copyright (C) Arduino s.r.l. and/or its affiliated companies
-# #
-# # SPDX-License-Identifier: MPL-2.0
-# from arduino.app_utils import *
-# from arduino.app_bricks.keyword_spotting import KeywordSpotting
-
-# spotter = KeywordSpotting()
-
-# def turn_on():
-#     Bridge.call("light_on")
-
-# def turn_off():
-#     Bridge.call("light_off")
-
-# spotter.on_detect("hey_arduino_turn_on_light", turn_on)
-# spotter.on_detect("hey_arduino_turn_off_light", turn_off)
-
-# App.run()
-
-
-
-
-
-
-
 from arduino.app_utils import *
 from arduino.app_bricks.keyword_spotting import KeywordSpotting
 
